61f8a1f3c18d639e269121c5eea37303bf309a33
Joseph <riverfish@gmail.com>
2017-05-13 19:22:30 +0200

    Connect redux and react-router-dom

--- src/index.js
+++ src/index.js
@@ -1,9 +1,29 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './components/app';
-require('./style.css');
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+
+import reducers from './reducers';
+require('./style.scss');
+import Home from './components/home';
+import About from './components/about';
+import Work from './components/work';
+import Contact from './components/contact';
+
+
+const createStoreWithMiddleware = applyMiddleware()(createStore);
 
 ReactDOM.render(
-  <App />,
+  <Provider store={createStoreWithMiddleware(reducers)}>
+    <BrowserRouter>
+      <Switch>
+        <Route path='/contact' component={Contact} />
+        <Route path='/work' component={Work} />
+        <Route path='/about' component={About} />
+        <Route path='/' component={Home} />
+      </Switch>
+    </BrowserRouter>
+  </Provider>,
   document.querySelector('#app')
 );
