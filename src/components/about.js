36af9966336a47935298954c5b2cdb92452a1a5b
Joseph <riverfish@gmail.com>
2018-08-31 21:14:08 -0400

    change navbar css

--- src/components/about.js
+++ src/components/about.js
@@ -9,7 +9,7 @@ const About = () => {
       <h1>Joseph Liu</h1>
       <div className='bio'>
         <p>Hi. I'm currently a Master's student in Computer and Information Technology (MCIT) at the University of Pennsylvania.</p>
-        <p>Previously, I completed medical school at Imperial College in London and practiced as a doctor for a few years afterwards.</p>
+        <p>Previously, I completed medical school at Imperial College in London and practised as a doctor for a few years afterwards.</p>
         <p>Along the way, I've also formulated dozens of supplements, interned at a few health tech startups, created an Amazon business and even worked as a cook at one point!</p>
         <p>What unites my previous experiences is a love for making things. Especially things that can have great impact.</p>
       </div>
